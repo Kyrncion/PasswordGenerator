e390fe24441b842623478d93603735ecc10ac139
@Kyrncion - Orion Developer <77999465+Kyrncion@users.noreply.github.com>
2021-11-05 21:42:41 -0700

    Add files via upload

---
+++ passwordgenerator.py
@@ -0,0 +1,17 @@
+# An Extremly Symple Password Generator Using Python
+
+import random
+
+Lower = "abcdefghijklmnopqrstuvwxyz"
+Upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+Number = "0123456789"
+Symbol = "[]{}#()*;._-"
+
+all = Lower + Upper + Number + Symbol
+
+length = 25 # Change Value To The Number Of Characters You Want Your Password To Contain
+
+password = "".join ( random.sample ( all,length ) )
+print ( " Here is your randomly generated passowrd: " , password )
+
+# Run Command By Typing 'passgen.py' In The Terminal
